698a4b6f75107e5efaae7fe1ee776da102039ed4
Marek294 <Markra5835@gmail.com>
2018-05-14 11:38:04 +0200

    Default Values for function parameters in ES6

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-function varFunc() {
-    var previous = 0;
-    var current = 1;
-    var i;
-    var temp;
+let receive = (complete = () => console.log('complete')) => complete();
 
-    for(i = 0; i < 10; i += 1) {
-        temp = previous;
-        previous = current;
-        current = temp + current;
-    }
-
-    console.log(current);
-}
-
-function letFunc() {
-    let previous = 0;
-    let current = 1;
-
-    for(let i = 0; i < 10; i += 1) {
-        let temp = previous;
-        previous = current;
-        current = temp + current;
-    }
-
-    console.log(current);
-}
-
-console.log("varFunc");
-varFunc();
-
-console.log("letFunc");
-letFunc();
+receive();
