977cfd43f06888ec07d157046de118500c7ce6a4
Marek294 <Markra5835@gmail.com>
2018-05-14 12:35:37 +0200

    Destructuring Assignment in ES6

--- app.js
+++ app.js
@@ -1,32 +1,69 @@
-var salutation = 'Hello';
-var place = 'planet';
-var greeting = `
-    You
-       Crazy    ${place}
-       
-       How
-    Are
-        You
-
-
-`;
-
-console.log(greeting);
-
-var x = 1;
-var y = 2;
-var equation = `${x} + ${y} = ${x+y}`;
-
-console.log(equation);
-
-function tag(strings, ...values) {
-    if(values[0] < 20) {
-        values[1] = 'awake';
-    }
-
-    return `${strings[0]}${values[0]}${strings[1]}${values[1]}`
+var { color, position } = {
+    color: 'blue',
+    name: 'John',
+    state: 'New York',
+    position: 'Forward'
 }
 
-var message = tag`It's ${new Date().getHours()} I'm ${''}`;
+console.log(color);
+console.log(position);
 
-console.log(message);
+function generateObj() {
+    return {
+        color: 'blue',
+        name: 'John',
+        state: 'New York',
+        position: 'Forward'
+    }
+};
+
+var {name: firstName, state: loc} = generateObj();
+
+console.log(firstName);
+console.log(loc);
+
+var [first,,,,fifth] = ['red', 'yellow', 'green', 'blue', 'orange'];
+
+console.log(first);
+console.log(fifth);
+
+var people = [
+    {
+      "firstName": "Clinton",
+      "lastName": "Ruiz",
+      "phone": "1-403-985-0449",
+      "email": "pharetra@facilisislorem.org",
+      "address": "Ap #829-3443 Nec St."
+    },
+    {
+      "firstName": "Skyler",
+      "lastName": "Carroll",
+      "phone": "1-429-754-5027",
+      "email": "Cras.vehicula.alique@diamProin.ca",
+      "address": "P.O. Box 171, 1135 Feugiat St."
+    },
+    {
+      "firstName": "Kylynn",
+      "lastName": "Madden",
+      "phone": "1-637-627-2810",
+      "email": "mollis.Duis@ante.co.uk",
+      "address": "993-6353 Aliquet, Street"
+    },
+    {
+      "firstName": "Chaney",
+      "lastName": "Edwards",
+      "phone": "1-397-181-4501",
+      "email": "rutrum@Nullamlobortis.net",
+      "address": "P.O. Box 342, 9574 Egestas Street"
+    }
+  ]
+
+  people.forEach(({firstName}) => console.log(firstName));
+
+  var [,Skyler] = people;
+
+  function logEmail({email}) {
+      console.log(email);
+  }
+
+  logEmail(Skyler);
