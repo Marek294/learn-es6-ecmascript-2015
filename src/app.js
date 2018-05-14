8ffcae6022e6fda9eed33275e733e809ce92f205
Marek294 <Markra5835@gmail.com>
2018-05-14 14:06:34 +0200

    Maps and WeakMaps

--- src/app.js
+++ src/app.js
@@ -1,32 +1,25 @@
-function* greet() {
-  let friendly = yield "How";
-  friendly = yield friendly + "Are";
-  yield friendly + "You";
+var myMap = new Map();
+
+var myObj = {};
+var myFunc = function(){};
+
+myMap.set(myObj, 'bar');
+myMap.set(myFunc, 'world');
+myMap.set('string', 2);
+
+console.log('get on myMap = ' + myMap.get(myObj))
+console.log('has on non-existing key = ' + myMap.has('qwerty'))
+
+for(var [key, value] of myMap.entries()){
+  console.log(key + ' = ' + value)
 }
 
-let greeter = greet();
-console.log(greeter.next().value);
-console.log(greeter.next(" asdasd ").value);
-console.log(greeter.next(" hi ").value);
+var myWeakMap = new WeakMap();
 
-function* graph() {
-  let x = 0;
-  let y = 0;
-  while(true) {
-    yield {x, y};
-    x += 2;
-    y += 1;
-  }
-}
+var myObj2 = {};
+var myFunc2 = function(){};
 
-var graphGenerator = graph();
-console.log(graphGenerator.next().value);
-console.log(graphGenerator.next().value);
-console.log(graphGenerator.next().value);
-console.log(graphGenerator.next().value);
-console.log(graphGenerator.next().value);
-console.log(graphGenerator.next().value);
-console.log(graphGenerator.next().value);
-console.log(graphGenerator.next().value);
-console.log(graphGenerator.next().value);
-console.log(graphGenerator.next().value);
+myMap.set(myObj2, 'bar');
+myMap.set(myFunc2, 'world');
+
+console.log(myMap.get(myFunc2))
