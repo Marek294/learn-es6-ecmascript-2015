ffbad14050be9ef0d2f2ed9e30cf9dcb9bae98f2
Marek294 <Markra5835@gmail.com>
2018-05-14 13:25:03 +0200

    Convert Array like object with Array.from()

--- src/app.js
+++ src/app.js
@@ -1,16 +1,5 @@
-import * as _ from 'lodash';
+const products = Array.from(document.querySelectorAll('.product'));
 
-import * as addition from 'math/addition';
-import {users} from 'data/users';
-
-console.log(_.where(users, {age: 36}));
-
-console.log(
-  "1 + 5",
-addition.sumTwo(1, 5)
-);
-
-console.log(
-  "1 + 9 + 7",
-  addition.sumThree(1, 9, 7)
-);
+products
+  .filter(item => parseFloat(item.innerHTML) < 10)
+  .forEach(item => item.style.color = 'red')
